08757deda7ec0a68410c4c44f4d805e3359a6a85
chetanp2402 <chetanp2402@gmail.com>
2022-07-04 09:54:33 +0530

    Added Kunal's approach for cyclic sort

--- src/com/chetan/sorting/CyclicSort.java
+++ src/com/chetan/sorting/CyclicSort.java
@@ -7,10 +7,12 @@ public class CyclicSort {
 	public static void main(String[] args) {
 
 		int[] arr = {3,2,5,7,6,4,1};
-		System.out.println(Arrays.toString(cyclicSort(arr)));
+		//System.out.println(Arrays.toString(cyclicSort1(arr)));
+		System.out.println(Arrays.toString(cyclicSort2(arr)));
+
 	}
 	
-	static int[] cyclicSort(int[] arr) {
+	static int[] cyclicSort1(int[] arr) {
 		
 		for (int i = 0; i < arr.length; i++) {
 			while(arr[i] != i+1) {
@@ -27,4 +29,18 @@ public class CyclicSort {
 		
 		return arr;
 	}
+	
+	static int[] cyclicSort2(int[] arr){
+		int i = 0;
+		while(i<arr.length) {
+			int correct = arr[i] -1;
+			if(arr[i] != arr[correct]) {
+				swap(arr, i, correct);
+			}else {
+			i++;
+			}
+		}
+		return arr;		
+	}
+	
 }
