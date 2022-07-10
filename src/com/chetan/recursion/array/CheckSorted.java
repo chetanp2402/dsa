ff032a7fe3dd534a6dbf7f0bbdb4cf752bf65020
chetanp2402 <chetanp2402@gmail.com>
2022-07-10 21:43:52 +0530

    recursion problems

--- src/com/chetan/recursion/array/CheckSorted.java
+++ src/com/chetan/recursion/array/CheckSorted.java
@@ -2,24 +2,20 @@ package com.chetan.recursion.array;
 
 public class CheckSorted {
     public static void main(String[] args) {
-        int [] arr = {8,2,3,5};
-        System.out.println(checkSorted(arr));
+        int[] arr = {8, 2, 3, 5};
+        System.out.println(checkSorted(arr, 0));
     }
 
-    private static boolean checkSorted(int[] arr) {
-        return checkSortedHelper(arr, 0);
-    }
+    private static boolean checkSorted(int[] arr, int index) {
 
-    private static boolean checkSortedHelper(int[] arr, int index) {
-
-        if(index == arr.length-1){
+        if (index == arr.length - 1) {
             return true;
         }
-        if(arr[index] > arr[index+1]){
+        if (arr[index] > arr[index + 1]) {
             return false;
         }
 
-        return checkSortedHelper(arr, ++index);
+        return checkSorted(arr, ++index);
     }
 
 
--- src/com/chetan/recursion/array/LinearSearch.java
+++ src/com/chetan/recursion/array/LinearSearch.java
@@ -1,16 +1,17 @@
 package com.chetan.recursion.array;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LinearSearch {
     public static void main(String[] args) {
-        int[] arr = {8, 2, 3, 5};
-        System.out.println(search(arr,5));
+        int[] arr = {8, 5, 2, 3, 5};
+        System.out.println(search(arr, 5, 0));
+        System.out.println(searchAll(arr, 5, 0));
     }
 
-    private static int search(int[] arr, int target) {
-        return searchHelper(arr, target, 0);
-    }
 
-    private static int searchHelper(int[] arr, int target, int index) {
+    private static int search(int[] arr, int target, int index) {
 
         if (index == arr.length) {
             return -1;
@@ -19,8 +20,23 @@ public class LinearSearch {
             return index;
         }
 
-        return searchHelper(arr, target, ++index);
+        return search(arr, target, ++index);
     }
 
 
+    private static List<Integer> searchAll(int[] arr, int target, int index) {
+        List<Integer> list = new ArrayList<>();
+
+        if (index == arr.length) {
+            return list;
+        }
+        if (arr[index] == target) {
+            list.add(index);
+        }
+
+        List<Integer> integers = searchAll(arr, target, ++index);
+        list.addAll(integers);
+        return  list;
+
+    }
 }
