3ca457e66e4f831acc3cc250885c10c512c6aa87
chetanp2402 <chetanp2402@gmail.com>
2022-07-10 16:32:14 +0530

    refactor

--- src/com/chetan/recursion/ReverseNumber.java
+++ src/com/chetan/recursion/ReverseNumber.java
@@ -2,17 +2,25 @@ package com.chetan.recursion;
 
 public class ReverseNumber {
     public static void main(String[] args) {
-        int reverseNumber = reverseNumber(123456, findNoOfDigits(123456));
+        int reverseNumber = reverseNumber(1212);
         System.out.println(reverseNumber);
+
+        System.out.println(isPalindrome(1212));
     }
 
-    static int reverseNumber(int n, int noOfDigits) {
+    static int reverseNumber(int n){
+        return helperFunction(n, findNoOfDigits(n));
+    }
+
+    static boolean isPalindrome(int n){
+        return n == reverseNumber(n);
+    }
+    static int helperFunction(int n, int noOfDigits) {
 
         if (n < 10){
             return n;
         }
-
-            return (n%10) * (int)Math.pow(10,noOfDigits-1) + reverseNumber(n/10,--noOfDigits);
+        return (n%10) * (int)Math.pow(10,noOfDigits-1) + helperFunction(n/10,--noOfDigits);
     }
 
     static int findNoOfDigits(int n) {
