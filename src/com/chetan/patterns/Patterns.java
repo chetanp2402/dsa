29100f94315b524a3fc5c486f1040bb6303cf5af
chetanp2402 <chetanp2402@gmail.com>
2022-07-13 21:53:42 +0530

    patterns - variables renamed

--- src/com/chetan/patterns/Patterns.java
+++ src/com/chetan/patterns/Patterns.java
@@ -83,9 +83,9 @@ public class Patterns {
             *       */
 
     static void pattern5(int n){
-        for (int i = 0; i < 2* n; i++) {
-            int totalColsInRow = i>n ? 2*n -i :i;
-            for (int j = 0; j < totalColsInRow; j++) {
+        for (int row = 0; row < 2* n; row++) {
+            int totalColsInRow = row>n ? 2*n -row :row;
+            for (int col = 0; col < totalColsInRow; col++) {
                     System.out.print("* ");
                 }
             System.out.println();
