4887beb8850a213ddaa52df67548a5571dbab8fa
chetanp2402 <chetanp2402@gmail.com>
2022-07-16 12:28:17 +0530

    patterns recursion

--- src/com/chetan/recursion/pattern/Triangle.java
+++ src/com/chetan/recursion/pattern/Triangle.java
@@ -3,8 +3,9 @@ package com.chetan.recursion.pattern;
 public class Triangle {
 
     public static void main(String[] args) {
-       // pattern1(5, 0);
-        pattern2(5,0,5);
+//       pattern1(5, 0);
+//        pattern2(5,0,5);
+        pattern2Way2(5,0);
     }
 
     static void pattern1(int row, int col) {
@@ -22,6 +23,21 @@ public class Triangle {
 
     }
 
+
+    static void pattern2Way2(int row, int col) {
+
+        if (row == 0) {
+            return;
+        }
+        if (col < row) {
+            pattern2Way2(row, ++col);
+            System.out.print("*");
+        } else {
+            pattern2Way2(--row, 0);
+            System.out.println();
+        }
+
+    }
     static void pattern2(int row, int col, int n) {
 
         if (row == 0) {
