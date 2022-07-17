bc8baae0cd9e5d8c43d00a6ce873c5fe5d1e76af
chetanp2402 <chetanp2402@gmail.com>
2022-07-17 17:14:26 +0530

    recursion skip char - refactored for debug

--- src/com/chetan/recursion/subset/SkipCharacter.java
+++ src/com/chetan/recursion/subset/SkipCharacter.java
@@ -4,39 +4,42 @@ public class SkipCharacter {
 
     public static void main(String[] args) {
         String input = "baccad";
-//        System.out.println(skipCharacter(input, new String()));
-        String ans = skipCharacterWithOneInput(input);
-        System.out.println(ans);
+        System.out.println(skipCharacter(input, new String()));
+//        String ans = skipCharacterWithOneInput(input);
+//        System.out.println(ans);
 
     }
 
-    static String skipCharacter(String input, String output){
+    static String skipCharacter(String input, String output) {
 
-        if(input.isEmpty()){
+        if (input.isEmpty()) {
             return output;
         }
 
-        if(input.charAt(0) != 'a'){
-            return skipCharacter(input.substring(1),output+input.charAt(0));
+        char ch = input.charAt(0);
+        if (ch != 'a') {
+            String ans = skipCharacter(input.substring(1), output + ch);
+            return ans;
         }
 
-        return skipCharacter(input.substring(1),output);
+        String ans = skipCharacter(input.substring(1), output);
+        return ans;
 
     }
 
 
-    static String skipCharacterWithOneInput(String input){
+    static String skipCharacterWithOneInput(String input) {
 
-        if(input.isEmpty()){
+        if (input.isEmpty()) {
             return "";
         }
         char ch = input.charAt(0);
-        if( ch != 'a'){
+        if (ch != 'a') {
             String ans = ch + skipCharacterWithOneInput(input.substring(1));
-            return  ans;
+            return ans;
         }
         String ans = skipCharacterWithOneInput(input.substring(1));
-        return ans  ;
+        return ans;
 
     }
 }
