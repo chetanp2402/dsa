884325fd2011588c33b1d3a33db905e7966fabf3
chetanp2402 <chetanp2402@gmail.com>
2022-07-23 21:45:41 +0530

    Permutation count

--- src/com/chetan/recursion/subset/Permutations.java
+++ src/com/chetan/recursion/subset/Permutations.java
@@ -7,9 +7,9 @@ public class Permutations {
 
     public static void main(String[] args) {
 
-       // permutations("", "abc");
-        System.out.println(permutationsGetList("", "abc"));
-
+       //permutations("", "abc");
+        //System.out.println(permutationsGetList("", "abc"));
+        System.out.println(permutationsCount("", "abc"));
     }
 
     static void permutations(String p, String up){
@@ -44,4 +44,22 @@ public class Permutations {
         }
         return  ans;
     }
+
+
+    static int permutationsCount(String p, String up){
+        if(up.isEmpty()){
+
+            return 1;
+        }
+
+        char ch = up.charAt(0);
+        int count =0;
+        for (int i = 0; i <= p.length(); i++) {
+            String first = p.substring(0,i);
+            String second = p.substring(i,p.length());
+            count = count + permutationsCount(first + ch + second, up.substring(1));
+        }
+        return count;
+    }
+
 }
